7d3a811dc29ea364579cf1b3a9d5ebb9147f70ae
Abiodun001-world <adekunleabiodun354@gmail.com>
2023-03-13 21:37:43 -0700

    changing the variabl count in the console to var argumentsPassed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
+
 // a script that prints boolean of each time the arguments found
 
 const argumentsPassed = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+console.log(argumentsPassed === 2 ? 'No argument' : argumentsPassed === 3 ? 'Argument found' : 'Arguments found');
